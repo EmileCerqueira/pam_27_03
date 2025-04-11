f330003e556b6a89d42226af17026ec8e9cde613
Emile <emile.bispo@etec.sp.gov.br>
2025-04-10 21:59:14 -0300

    new

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,28 @@
 import CustomButton from "@/components/Title";
 import React, { useState } from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
 
 export default function HomeScreen() {
+    const name = "Emile";
+
     const [count, setCount] = useState(0);
+    const [nome, setNome] = useState (name);
 
     
 
     return(
-        <View>
-            <Text>Bem-vindo ao meu App</Text>
+        <View style={{flex: 1, justifyContent:"center", alignItems:"center"}}>
+            <Text>Bem-vindo ao App {nome}!</Text>
+
             <Text>Contador: {count}</Text>
             <Button title="Aumentar" onPress={() => {setCount(count + 1)}}/>
             <Button title="Diminuir" onPress={() => {setCount(count - 1)}}/>
             <Button title="Resetar" onPress={() => {setCount(- 0)}}/>
-            <CustomButton title="NAME"/>
+
+        <TextInput placeholder="Digite Algo" onChangeText={setNome}/>
+
+            <CustomButton title="Botão vermelho" onPress={() =>{setNome("Emile" + count)}}/>
+                
         </View>
     );
 
